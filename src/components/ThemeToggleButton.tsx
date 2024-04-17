7d768a1afa26ed1e0d0a60877207510521fafd5f
“Zhengfei <xiangzhengfei@outlook.com>
2024-04-17 18:35:35 -0400

    lint

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,37 +1,37 @@
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
-import { IconButton, IconButtonProps, useColorMode } from '@chakra-ui/react';
-import styled from '@emotion/styled';
+// import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+// import { IconButton, IconButtonProps, useColorMode } from '@chakra-ui/react';
+// import styled from '@emotion/styled';
 
-import transientOptions from '../utils/general';
+// import transientOptions from '../utils/general';
 
-// PROP TYPES
-type ThemeToggleButtonProps = Omit<IconButtonProps, 'aria-label'>;
+// // PROP TYPES
+// type ThemeToggleButtonProps = Omit<IconButtonProps, 'aria-label'>;
 
-// CONSTS and LETS
-const iconSize = 20;
+// // CONSTS and LETS
+// const iconSize = 20;
 
-const RoundButton = styled(IconButton, transientOptions)`
-  box-shadow: 0 0 100px 20px
-    ${({ $colorMode }) => ($colorMode === 'light' ? 'black' : 'white')};
-  & svg {
-    width: ${iconSize}px;
-    height: ${iconSize}px;
-  }
-`;
+// const RoundButton = styled(IconButton, transientOptions)`
+//   box-shadow: 0 0 100px 20px
+//     ${({ $colorMode }) => ($colorMode === 'light' ? 'black' : 'white')};
+//   & svg {
+//     width: ${iconSize}px;
+//     height: ${iconSize}px;
+//   }
+// `;
 
-function ThemeToggleButton(props: ThemeToggleButtonProps): JSX.Element {
-  const { colorMode, toggleColorMode } = useColorMode();
+// function ThemeToggleButton(props: ThemeToggleButtonProps): JSX.Element {
+//   const { colorMode, toggleColorMode } = useColorMode();
 
-  return (
-    <RoundButton
-      $colorMode={colorMode}
-      onClick={toggleColorMode}
-      icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-      aria-label={`Activate ${colorMode === 'light' ? 'dark' : 'light'} mode`}
-      isRound
-      {...props}
-    />
-  );
-}
+//   return (
+//     <RoundButton
+//       $colorMode={colorMode}
+//       onClick={toggleColorMode}
+//       icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+//       aria-label={`Activate ${colorMode === 'light' ? 'dark' : 'light'} mode`}
+//       isRound
+//       {...props}
+//     />
+//   );
+// }
 
-export default ThemeToggleButton;
+// export default ThemeToggleButton;
